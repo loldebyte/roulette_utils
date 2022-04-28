4f09d226ffc2b2871ce9f861073822b2724456ca
loldebyte <qdimarellis@laposte.net>
2022-04-28 11:20:32 +0200

    refacto: change 50 roll code to be parametrable

--- avg_unique_numbers_after_n_rolls.py
+++ avg_unique_numbers_after_n_rolls.py
@@ -16,5 +16,15 @@ def roll_50_times() -> int:
     rolled_numbers = set([roll() for _ in range(50)])
     return len(rolled_numbers)
 
+def roll_n_times(n: int) -> int:
+    rolled_numbers = set([roll() for _ in range(n)])
+    return len(rolled_numbers)
+
+"""
 vals = [roll_50_times() for _ in range(100000)]
 print(statistics.mean(vals))
+"""
+
+for k in range(5, 9):
+    vals = [roll_n_times(80) for _ in range(100000)]
+    print(len(filter(lambda x: x > k, vals)))
