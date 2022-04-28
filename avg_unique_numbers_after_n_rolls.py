8858fb95595f3bad478fcbcbc1bf11a4a7d12e2a
loldebyte <qdimarellis@laposte.net>
2022-04-28 11:35:09 +0200

    feat: make output much clearer
    Add # of series considered, length of series considered & # of remaining numbers considered

--- avg_unique_numbers_after_n_rolls.py
+++ avg_unique_numbers_after_n_rolls.py
@@ -6,8 +6,6 @@ Created on Thu Apr 28 00:43:54 2022
 @author: loldebyte
 """
 import random
-import statistics
-
 
 def roll() -> int:
     return random.randint(0, 36)
@@ -20,11 +18,15 @@ def roll_n_times(n: int) -> int:
     rolled_numbers = set([roll() for _ in range(n)])
     return len(rolled_numbers)
 
+N_TEST = 100000
+N = 80
+
 """
 vals = [roll_50_times() for _ in range(100000)]
 print(statistics.mean(vals))
 """
 
 for k in range(5, 9):
-    vals = [roll_n_times(80) for _ in range(100000)]
-    print(len(filter(lambda x: x > k, vals)))
+    vals = [roll_n_times(N) for _ in range(N_TEST)]
+    n_successful = len(list(filter(lambda x: x+k <= 37, vals)))
+    print(f"% series where {k} numbers never rolled in a series of {N} rolls : {n_successful/N_TEST*100} ; avg out of {N_TEST} series")
